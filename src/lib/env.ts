b8cd549ba968fddc8c24b1d9cae38ae888bb76ad
Clement <kwasiclement764@gmail.com>
2025-10-01 21:42:20 +0000

    Update env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -7,6 +7,9 @@ export const MONGODB_URI = process.env.MONGODB_URI || "";
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || "";
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "";
 
+export const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || "";
+export const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || "";
+
 export const NEXT_PUBLIC_PADDLE_CLIENT_TOKEN = process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN || "";
 
 export const PADDLE_API_KEY = process.env.PADDLE_API_KEY || "";
